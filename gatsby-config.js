a413da6d12fa39a8c091086f8379591ef983e2ff
James Toney <eighty303solutions@jamess-mbp-2.attlocal.net>
2018-02-05 11:08:00 -0600

    added analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
-                //trackingId: `ADD YOUR TRACKING ID HERE`,
+                trackingId: `UA-70342214-1`,
             },
         },
         `gatsby-plugin-feed`,
